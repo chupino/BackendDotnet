6a2c2d2bc5e002f1c445e72946c814730196adbc
Mauricio <mauriciodiego2019@gmail.com>
2024-08-31 16:18:25 -0500

    a

--- ApiController.cs
+++ ApiController.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 namespace Backend.Controllers
 {
     [Route("api")]
