565e8132b7eaca65d0c101267c2264987bb9f646
chupino <mauricioescalante903@gmail.com>
2024-09-01 14:22:01 -0500

    a

--- dotnet.sh
+++ dotnet.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-dotnet ef migrations add Initial
+# Variables
+MIGRATION_NAME="Initial"
+
+# Verificar si la migración ya existe
+if dotnet ef migrations list | grep -q "$MIGRATION_NAME"; then
+    echo "La migración '$MIGRATION_NAME' ya existe."
+else
+    echo "Agregando la migración '$MIGRATION_NAME'."
+    dotnet ef migrations add "$MIGRATION_NAME"
+fi
+
+# Aplicar migraciones a la base de datos
+echo "Aplicando migraciones a la base de datos."
 dotnet ef database update
+
+# Iniciar la aplicación
+echo "Iniciando la aplicación."
 dotnet run --urls "http://0.0.0.0:80"
+
