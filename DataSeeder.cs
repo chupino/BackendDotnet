27f844472f8cd69bd8d46c9bdd968eb2be313330
Mauricio <mauriciodiego2019@gmail.com>
2024-08-31 20:32:05 -0500

    a

--- DataSeeder.cs
+++ DataSeeder.cs
@@ -3,7 +3,7 @@ public static class DataSeeder
 {
     public static void Seed(ModelBuilder modelBuilder)
     {
-        var baseStorageUrl="http://ip172-18-0-41-cr9r9e2im2rg00fl4om0-6000.direct.labs.play-with-docker.com/"
+        var baseStorageUrl="http://ip172-18-0-41-cr9r9e2im2rg00fl4om0-6000.direct.labs.play-with-docker.com/";
         var htmlFiles = new List<HTMLFile>();
         for (int i = 1; i <= 8; i++)
         {
